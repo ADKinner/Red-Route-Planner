c378e75952e7d9db14e663231560597aa3ec378f
DzmitryKuhach750503 <dkaezi@gmail.com>
2019-11-24 11:26:51 +0300

    Change colors, icon of app and its name

--- app/src/main/java/com/example/redrouteplanner/RouteActivity.java
+++ app/src/main/java/com/example/redrouteplanner/RouteActivity.java
@@ -3,6 +3,8 @@ package com.example.redrouteplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
